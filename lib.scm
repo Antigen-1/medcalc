38885ebeb5d505a743121e3ab804336a37edb6a1
haozhang <haozhang@antigen.top>
2025-12-28 21:51:51 +0800

    Fix mul

--- lib.scm
+++ lib.scm
@@ -2,7 +2,7 @@
 (define <= (#%vm-procedure (=> (dynamic-require "operator" none) "le") 2))
 (define >= (#%vm-procedure (=> (dynamic-require "operator" none) "ge") 2))
 
-(define mul (make-procedure (lambda (l) (let ((len (length l))) (let loop ((i 0)) (if (equal? i l) 1 (* (@ l i) (loop (+ i 1)))))))))
+(define mul (make-procedure (lambda (l) (let ((len (length l))) (let loop ((i 0)) (if (equal? i len) 1 (* (@ l i) (loop (+ i 1)))))))))
 
 (define min (lambda (n1 n2) (if (< n1 n2) n1 n2)))
 (define max (lambda (n1 n2) (if (< n1 n2) n2 n1)))
