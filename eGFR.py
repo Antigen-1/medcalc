4eb33a6dd258e43b0e55cdeb329b7f0c64c6f769
haozhang <haozhang@antigen.top>
2025-12-28 15:21:58 +0800

    Fix paths

--- eGFR.py
+++ eGFR.py
@@ -1,5 +1,6 @@
 import core
+import os.path
 
 eGFR = None
-with open("eGFR.json", "rt") as f:
+with open(os.path.join(os.path.dirname(__file__), "eGFR.json"), "rt") as f:
     eGFR = core.run(f.read())
